b5ba064c6da2dde2a195735f28b540a0f4fe2023
Evgeniia Mitrakhova <e.mitrakhova@cy.cardpay.com>
2018-02-16 18:37:03 +1000

    edit tdd file, write to files, convert into html

--- utils/jsonToHtml.js
+++ utils/jsonToHtml.js
@@ -1,22 +1,31 @@
 var tddJsonConfig = require('../config/config.json');
 var path = require('path');
 var fs = require('fs');
+var ftl2html = require('ftl2html');
 
 /**
  * @description convert json file into required html 
- * @param {*} pathToTheme 
- * @param {*} currentFile 
+ * @param {string} pathToTheme - path to linked theme in our project
+ * @param {string} currentFile - filename for converting
  */
 module.exports = function jsonToHtml(pathToTheme, currentFile) {
-  if (typeof pathToTheme !== "string" || typeof currentFile !== "object") {
+  if (typeof pathToTheme !== "string" || typeof currentFile !== "string") {
     throw new Error('jsonToHtml: Invalid path to theme');
   }
   fs.readFile(path.resolve(pathToTheme, 'login/messages/messages_en.properties'), 'utf-8', (err, data) => {
     if (!!err) {
       throw new Error(err);
     } 
+    // Preparations for generating
     var msg = parsePropFile(data);
-    var jsonFile = createTddFile(tddJsonConfig, msg, currentFile);
+    var ftlVar = createTddFile(tddJsonConfig, msg, currentFile);
+    // ftlVar = generateConfiguration(ftlVar);
+    // Generate HTML file
+    // viewRoot - root for ftl files
+    fs.writeFile(path.join(__dirname, '../utils/tdd', currentFile + '.tdd'), JSON.stringify(ftlVar,null, 4), () => {
+      ftl2html(path.resolve(pathToTheme, 'login') , path.join(__dirname, '../dist/html/') , currentFile + '.ftl', path.join(__dirname, '../utils/tdd/', currentFile + '.tdd'), 'logFile');
+    });  
+    
   });
 };
 
@@ -44,17 +53,28 @@ function parsePropFile(filecontent) {
   return msg;
 }
 
+/**
+ * @description create tdd file from: defult jsonConfig file, 
+ * generated from message_en.properties msgBlock JSON, filename 
+ * @param {*} jsonConfig - default config file with variables
+ * @param {*} msgBlock - generated msg block
+ * @param {string} requiredFile - required filename
+ */
 function createTddFile(jsonConfig, msgBlock, requiredFile) {
-  // if (!!jsonConfig || typeof msgBlock !== "object" || typeof requiredFile !== "object") {
-  //   throw new Error('cannot create tdd file');
-  // }
+  if (!jsonConfig || !msgBlock || !requiredFile) {
+    throw new Error('Invalid parameters. Impossible to create tdd file');
+  }
   var json = JSON.parse(JSON.stringify(jsonConfig));
   var template = json.template;
-  // console.log(requiredFile.formName);
-  var requiredPage = json[requiredFile.formName];
-  var requiredMode = requiredPage[requiredFile.formMode];
-  // var resultJSON = JSON.stringify(template.concat(requiredMode,)).concat(JSON.stringify, JSON.stringify(msgBlock));
-  requiredMode.template = template;
-  requiredMode.msg = msgBlock;
-  console.log(JSON.stringify(requiredMode));
+  var requiredPage = json[requiredFile];
+  //TODO: избавиться от строки ниже после появления аяксов
+  var requiredMode = requiredPage['error'];
+  Object.keys(template).forEach(elem => {
+    requiredMode[elem] = template[elem];
+  });
+  Object.keys(msgBlock).forEach(elem => {
+    requiredMode[elem] = msgBlock[elem];
+  });
+  
+  return requiredMode;
 }
